9596d6f5f326572cb0bf1ab2142a6d03139a97cf
Santiago M Gonzalez <a24310321@live.ceti.mx>
2025-09-05 01:27:02 +0000

    Diagrama

---
+++  main.cpp
@@ -0,0 +1,11 @@
+#include <Foco.hpp>
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    cout << "Hola Mundo" << endl;
+    Foco foco;
+    foco.Encender();
+    cout << "EstadoFoco: " << foco.LeerEstado() << endl;
+    return 0;
+} 
