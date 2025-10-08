eb6d1129e591bd7a55098b0d89f176e97bb78930
Santiago M Gonzalez <a24310321@live.ceti.mx>
2025-10-08 02:53:33 +0000

    Practica 2

---
+++ src/memoria.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+class Persona{
+public:
+        char nombre[10];
+        int edad;
+};
+
+class Empleado : public Persona{
+public:
+        int noEmpleado;
+};
+
+class Alumno : public Persona{
+public:
+        int registro;
+};
+
+int main(int argc, char const *argv[])
+{
+        cout << "Tamaño de los tipos de datos" << endl;
+        cout <<  "Tamaño int: " << sizeof(int) << endl;
+        cout <<  "Tamaño long: " << sizeof(long) << endl;
+        cout <<  "Tamaño float: " << sizeof(float) << endl;
+        cout <<  "Tamaño double: " << sizeof(double) << endl;
+        cout <<  "Tamaño bool: " << sizeof(bool) << endl;
+        cout <<  "Tamaño char: " << sizeof(char) << endl;
+        cout <<  "Tamaño byte: " << sizeof(byte) << endl;
+        
+        cout << "Direcciones de memoria " << endl;
+        cout << "Punteros en C: " << malloc(sizeof(int)) << endl;
+        cout << "Punteros en C++: " << new int << endl;
+        
+        cout << "Tamaño de los punteros: " << endl;
+        cout << "int* " << sizeof(int*) << endl;
+        cout << "char* " << sizeof(char*) << endl;
+
+        cout << "Casteo de tipos" << endl;
+        //casteo de punteros
+        int* puntero = (int*)malloc(sizeof(int));
+
+        //Casteo de variables
+        int ejemplo = 65;
+        char letra = (char)ejemplo;
+         cout << letra << endl;
+
+         //casteo Polimorfismo
+         Persona* persona = new Empleado():
+         Persona* persona2 = new Alumno();
+
+         list<Persona*> personas;
+         personas.emplace_back(persona);
+         personas.emplace_back(persona2);
+
+         cout << (*persona).edad << endl;
+         cout << (*persona).nombre << endl;
+         cout << (*persona).noEmpleado << endl;
+         cout << persona->noEmpleado << endl;
+         cout << persona->edad << endl;
+         cout << persona->nombre << endl;
+         
+
+
+
+        
+        
+        return 0;
+}
